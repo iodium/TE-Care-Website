1afb9c789ca4ef9dd4d9a423ec853b42c91ee7ce
Tejas Nikam <tejas.nikam3100@gmail.com>
2026-08-27 11:54:14 -0400

    added more starter code

--- app.py
+++ app.py
@@ -6,7 +6,11 @@ app.secret_key = "key"
 
 @app.route('/')
 def home():
-    pass
+    return render_template("home.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
